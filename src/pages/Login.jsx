4d7c845a8637c71c33ef2a9179a54b8e02205916
malcomowilla <owillamalcom@gmail.com>
2023-12-19 15:42:15 +0300

    vercel

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -42,7 +42,7 @@ const handleLoginWithGoogle = ()=> {
   <div className="hero-overlay bg-opacity-60"></div>
   <video controls autoPlay loop className="object-cover w-full h-full"  >
 
-<source src="../assets/istockphoto-1449327718-640_adpp_is.mp4" type="video/mp4"></source>
+<source src="/images/istockphoto-1449327718-640_adpp_is.mp4" type="video/mp4"></source>
         </video>
 
         {/* <audio controls className="hidden" autoPlay loop>
@@ -56,7 +56,7 @@ const handleLoginWithGoogle = ()=> {
       <h1 className="mb-5 text-[100px] font-bold text-white my-font">Hello there 👋🏻</h1>
       <p className="mb-5 text-white text-lg text">          Join the conversation, meet new people, and make connections in one shared room.
 .</p>
-      <button  onClick={handleLoginWithGoogle}  className="btn ">Login With Google <img  className="max-w-[20px]" src="../assets/search.png"/>
+      <button  onClick={handleLoginWithGoogle}  className="btn ">Login With Google <img  className="max-w-[20px]" src="/images/search.png"/>
 </button>
     </div>
   </div>
